3b0ab0f0d937b8d011b2644654a39c5a455678ae
Tú Nguyễn <nguyencongtu0343@gmail.com>
2022-04-28 08:33:03 +0700

    fix xiu

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using NaughtyAttributes;
+using Sirenix.OdinInspector;
 
 public class Test : MonoBehaviour
 {
+    [ShowInInspector] public Dictionary<int, int> dicTest = new Dictionary<int, int>();
+
     [Button("Test1 abc")]
     private void Test1()
     {
 
+    }
+
+    private void Start()
+    {
+        dicTest.Add(1, 1);
+        dicTest.Add(2, 2);
     }
 }
